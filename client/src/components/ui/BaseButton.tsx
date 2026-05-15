0647f3d38c3308e29c686f428e5de1fdf5976d4e
ze-nien <bb0145786932@gmail.com>
2026-05-15 19:20:26 +0800

    client:css / extract component(sidebar)

---
+++ client/src/components/ui/BaseButton.tsx
@@ -0,0 +1,40 @@
+interface BaseButtonProps {
+  options: readonly string[];
+  activeValue: string | readonly string[];
+  onClick: (value: string) => void;
+  className?: string;
+}
+
+const BaseButton = ({
+  options,
+  activeValue,
+  onClick,
+  className,
+}: BaseButtonProps) => {
+  return (
+    <div className={className}>
+      {options.map((option) => {
+        const isActive = Array.isArray(activeValue)
+          ? activeValue.includes(option)
+          : activeValue === option;
+        return (
+          <button
+            type="button"
+            key={option}
+            onClick={() => onClick(option)}
+            className={`text-xs py-0.5 rounded-md transition hover:cursor-pointer 
+                              ${
+                                isActive
+                                  ? "bg-secondary text-primary md:bg-primary md:text-secondary" // 選中樣式
+                                  : "hover:text-primary hover:bg-secondary md:hover:text-secondary md:hover:bg-primary" // 未選中樣式
+                              }`}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BaseButton;
