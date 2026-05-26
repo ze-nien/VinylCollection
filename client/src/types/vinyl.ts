b0a7c87209b1a50734369481aedba36e62005349
ze-nien <bb0145786932@gmail.com>
2026-05-26 20:40:21 +0800

    -

--- client/src/types/vinyl.ts
+++ client/src/types/vinyl.ts
@@ -1,5 +1,17 @@
 import { z } from "zod";
-import { vinylSchema } from "../../../shared/vinylSchema.js";
+import { GENRES } from "../../../shared/constants";
+// import { vinylSchema } from "../../../shared/vinylSchema.js";
+
+export const vinylSchema = z.object({
+  album: z.string(),
+  artist: z.string(),
+  genre: z.array(z.enum(GENRES)).optional(), // 如果有固定流派，可以寫 z.enum(["Rock", "Jazz"])
+  coverUrl: z.string().optional(),
+  version: z.string(),
+  year: z.number(),
+  albumRating: z.number(),
+  notes: z.string().optional(),
+});
 
 export type VinylBase = z.infer<typeof vinylSchema>;
 
