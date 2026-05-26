2bf5a36b01f9a427cc374cd2508cee82b3cf93a8
ze-nien <bb0145786932@gmail.com>
2026-05-26 20:34:00 +0800

    -

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from "../store/authStore";
 
 //建立axios實例
 const api = axios.create({
-  baseURL: "https://vinylcollection.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
   timeout: 10000, //發送請求超過10秒視為請求失敗
   withCredentials: true, //允許跨域請求攜帶並寫入Cookie
 });
